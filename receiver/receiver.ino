d377fdc6a09a048271990d9cf0724920438b1677
Jason Ashton <ashtonguitars@gmail.com>
2014-11-09 18:00:49 -0500

    Cleaned up code, added cases

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,11 +1,8 @@
-/*
-  RF_Sniffer
- 
-  Hacked from http://code.google.com/p/rc-switch/
- 
-  by @justy to provide a handy RF code sniffer
+/*Incoming signal will eventually be read as ### ## #, where the first three numbers identify the device, next two the appliance, and the last one the state.
+Here we only use the last three. 1 means off, 2 means on, because 0 is interpreted as an error (###)
+01# is Channel 1
+02# is Channel 2
 */
- 
 #include <RCSwitch.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,36 +10,35 @@ RCSwitch mySwitch = RCSwitch();
  
 void setup() {
   Serial.begin(9600);
-  mySwitch.enableReceive(0);  // Receiver on inerrupt 0 =&gt; that is pin #2
-  pinMode(7, OUTPUT);
-  Serial.print("I'm working");
+  mySwitch.enableReceive(0);  // Receiver on interrupt 0 =&gt; that is pin #2
+  pinMode(7, OUTPUT); // Channel 1 of Relay
+  pinMode(6, OUTPUT); // Channel 2 of Relay
 }
  
 void loop() {
   if (mySwitch.available()) {
  
     int value = mySwitch.getReceivedValue();
- 
-    if (value == 0) {
-      Serial.print("Unknown encoding");
-    } else {
-     Serial.print("Received ");
-      Serial.print( mySwitch.getReceivedValue() );
-      if(value == 2){
-        digitalWrite(7, HIGH);
-        Serial.print(" Switching ON");
-      }else if(value == 1){
+    
+    switch (value) {
+      case 0: //Error
+        Serial.print("Unknown encoding");
+        break;
+      case 11: //Channel 1 off
         digitalWrite(7, LOW);
-        Serial.print(" Switching OFF");
-      }
-      Serial.print(" / ");
-      Serial.print( mySwitch.getReceivedBitlength() );
-      Serial.print("bit ");
-      Serial.print("Protocol: ");
-      Serial.println( mySwitch.getReceivedProtocol() );
+        break;
+      case 12: //Channel 1 on
+        digitalWrite(7, HIGH);
+        break;
+      case 21: //Channel 2 off
+        digitalWrite(6, LOW);
+        break;
+      case 22: //Channel 2 on
+        digitalWrite(6, HIGH);
+        break;
+      default: //Something else? Do nothing.
+        break;
     }
- 
     mySwitch.resetAvailable();
- 
   }
 }
