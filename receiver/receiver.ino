cd0bc52313f353b041b0897ed1cdff2c82b90280
Jason Ashton <ashtonguitars@gmail.com>
2014-11-09 16:48:48 -0500

    Changed to the RC code

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,58 +1,48 @@
-// receiver.pde
-//
-// Simple example of how to use VirtualWire to receive messages
-// Implements a simplex (one-way) receiver with an Rx-B1 module
-//
-// See VirtualWire.h for detailed API docs
-// Author: Mike McCauley (mikem@airspayce.com)
-// Copyright (C) 2008 Mike McCauley
-// $Id: receiver.pde,v 1.3 2009/03/30 00:07:24 mikem Exp $
-
-#include <VirtualWire.h>
-int relay = 0;
-
-void setup()
-{
-    Serial.begin(9600);	// Debugging only
-    Serial.println("setup");
-    pinMode(7, OUTPUT);
-    
-
-
-    // Initialise the IO and ISR
-    vw_set_ptt_inverted(true); // Required for DR3100
-    vw_setup(2000);	 // Bits per sec
-
-    vw_rx_start();       // Start the receiver PLL running
+/*
+  RF_Sniffer
+ 
+  Hacked from http://code.google.com/p/rc-switch/
+ 
+  by @justy to provide a handy RF code sniffer
+*/
+ 
+#include <RCSwitch.h>
+#include <stdlib.h>
+#include <stdio.h>
+RCSwitch mySwitch = RCSwitch();
+ 
+void setup() {
+  Serial.begin(9600);
+  mySwitch.enableReceive(0);  // Receiver on inerrupt 0 =&gt; that is pin #2
+  pinMode(7, OUTPUT);
+  Serial.print("I'm working");
 }
-
-void loop()
-{
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
-
-    if (vw_get_message(buf, &buflen)) // Non-blocking
-    {
-	int i;
-
-        digitalWrite(13, true); // Flash a light to show received good message
-	// Message with a good checksum received, dump it.
-	Serial.print("Got: ");
-	
-	for (i = 0; i < buflen; i++)
-	{
-	    Serial.print(buf[i], HEX);
-	    Serial.print(" ");
-	}
-	Serial.println("");
-        digitalWrite(13, false);
-        
-        if(relay == 0){
+ 
+void loop() {
+  if (mySwitch.available()) {
+ 
+    int value = mySwitch.getReceivedValue();
+ 
+    if (value == 0) {
+      Serial.print("Unknown encoding");
+    } else {
+     Serial.print("Received ");
+      Serial.print( mySwitch.getReceivedValue() );
+      if(value == 2){
         digitalWrite(7, HIGH);
-        relay = 1;
-        }else{
+        Serial.print(" Switching ON");
+      }else if(value == 1){
         digitalWrite(7, LOW);
-        relay = 0;
-        }
+        Serial.print(" Switching OFF");
+      }
+      Serial.print(" / ");
+      Serial.print( mySwitch.getReceivedBitlength() );
+      Serial.print("bit ");
+      Serial.print("Protocol: ");
+      Serial.println( mySwitch.getReceivedProtocol() );
     }
+ 
+    mySwitch.resetAvailable();
+ 
+  }
 }
