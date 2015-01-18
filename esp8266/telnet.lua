18abcd2192687f33db93e64c18efb03b80ab48de
Jason Ashton <ashtonguitars@gmail.com>
2015-01-18 14:32:47 -0500

    cleaned up

---
+++ esp8266/telnet.lua
@@ -0,0 +1,14 @@
+gpio.mode(3,gpio.OUTPUT)
+-- create a server
+sv=net.createServer(net.TCP, 30)    -- 30s time out for a inactive client
+print("Starting Server")
+-- server listen on 23, if data received, print data to console, and send "hello world" to remote.
+sv:listen(23,function(c)
+    c:send("Connection Successful\n")
+    c:on("receive", function(c, payload) print(payload)
+    pinstate = tostring(payload):sub(1, -2) 
+    c:send("You said: " .. pinstate .. "\n")
+    
+    if pinstate == "on" then gpio.write(3,gpio.HIGH) elseif pinstate == "off" then gpio.write(3,gpio.LOW) end
+    end)
+    end)
