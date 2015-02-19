af26a0ce5fafee50e5854fcba4a0d34baff2b829
Jason Ashton <ashtonguitars@gmail.com>
2015-02-19 13:40:21 -0500

    added connection exception

--- control/telnet.py
+++ control/telnet.py
@@ -6,12 +6,17 @@ def sendpacket(ip, packet):
     TCP_IP = ip
     TCP_PORT = 23
     MESSAGE = packet.encode('utf-8')
+    
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((TCP_IP, TCP_PORT))
+        s.send(MESSAGE)
+        s.close()
+        return "Successful Sending"
+    
+    except OSError:
+        print("Error sending. Check your parameters and the device itself.")
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
-    s.send(MESSAGE)
-    s.close()
-    return "Successful Sending"
 
 def main(ip):
     print("What is the message?")
